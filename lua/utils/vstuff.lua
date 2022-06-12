af263918647bd4344ae5a6590a7ea3c384f553c7
micro <micro@gnu-box>
2022-06-13 00:50:31 +0100

    moved abbreviations to the top

--- lua/utils/vstuff.lua
+++ lua/utils/vstuff.lua
@@ -14,33 +14,6 @@ autocmd BufWritePre *.[ch] %s/\%$/\r/e
 autocmd BufWritePre * cal cursor(currPos[1], currPos[2])
 ]], false)
 
-
--- disable builtins plugins
-local disabled_built_ins = {
-    "netrw",
-    "netrwPlugin",
-    "netrwSettings",
-    "netrwFileHandlers",
-    "gzip",
-    "zip",
-    "zipPlugin",
-    "tar",
-    "tarPlugin",
-    "getscript",
-    "getscriptPlugin",
-    "vimball",
-    "vimballPlugin",
-    "2html_plugin",
-    "logipat",
-    "rrhelper",
-    "spellfile_plugin",
-    "matchit"
-}
-
-for _, plugin in pairs(disabled_built_ins) do
-    vim.g["loaded_" .. plugin] = 1
-end
-
 -- Abbreviations
 vim.cmd([[
 cnoreabbrev W! w!
@@ -55,6 +28,32 @@ cnoreabbrev W w
 cnoreabbrev Q q
 cnoreabbrev Qall qall
 ]])
+
+-- disable builtins plugins
+local disabled_built_ins = {
+  "netrw",
+  "netrwPlugin",
+  "netrwSettings",
+  "netrwFileHandlers",
+  "gzip",
+  "zip",
+  "zipPlugin",
+  "tar",
+  "tarPlugin",
+  "getscript",
+  "getscriptPlugin",
+  "vimball",
+  "vimballPlugin",
+  "2html_plugin",
+  "logipat",
+  "rrhelper",
+  "spellfile_plugin",
+  "matchit"
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+  vim.g["loaded_" .. plugin] = 1
+end
 
 -- Vertically center document when entering insert mode
 vim.api.nvim_exec([[ autocmd InsertEnter * norm zz ]], false)
