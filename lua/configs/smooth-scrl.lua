d1d0b238b2de00f57a4932c8ad52a350bc28c20a
micro <micro@gnu-box>
2022-06-12 05:21:57 +0100

    increased speed for smooth scrolling

--- lua/configs/smooth-scrl.lua
+++ lua/configs/smooth-scrl.lua
@@ -13,16 +13,16 @@ require('neoscroll').setup({
 local t = {}
 
 -- ('alt + u' for scrolling up , 'alt + d' for scrolling down)
-t['<A-u>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '300'}}
-t['<A-d>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '300'}}
+t['<A-u>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '250'}}
+t['<A-d>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '250'}}
 
 -- ('alt + k' for scrolling up , 'alt + j' for scrolling down)
 t['<A-k>'] = {'scroll', {'-vim.wo.scroll', 'false', '150', [['sine']]}}
 t['<A-j>'] = {'scroll', { 'vim.wo.scroll', 'false', '150', [['sine']]}}
 
 -- ('ctrl + b' for scrolling up , 'ctrl + f' for scrolling down)
-t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '300'}}
-t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '300'}}
+t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '250'}}
+t['<C-f>'] = {'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '250'}}
 
 -- When no easing function is provided the default easing function (in this case "quadratic") will be used
 t['zz'] = {'zz', {'200'}}
